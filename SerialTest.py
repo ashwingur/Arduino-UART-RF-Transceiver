92b14e7474fefeaf5a6534491275de3a34661285
Ashwin <ashwingur88@gmail.com>
2024-04-17 10:35:07 +1000

    Added COM port and Baud rate variable

--- SerialTest.py
+++ SerialTest.py
@@ -9,7 +9,12 @@ import threading
 import struct
 import csv
 
+COM_PORT = "COM4"
+BAUD_RATE = 115200
+
 # Function to continuously read data from serial port
+
+
 def serial_read(serial_port):
     additional_packets = b''
     reading_additional_data = False
@@ -31,12 +36,15 @@ def serial_read(serial_port):
             print(f'Decoded and stripped: {data.decode().strip()}\n')
 
 # Function to continuously send data to serial port
+
+
 def serial_write(serial_port):
     while True:
         message = input("")
         if not message:
             break
         serial_port.write(message.encode())
+
 
 def process_wod(packets):
     chunk_size = 64
@@ -49,7 +57,7 @@ def process_wod(packets):
         # packet = packets[chunk_size*i:(chunk_size*(i+1))]
         # unpacked_data = struct.unpack(data_format, packet)
         # print(f'Packets remaining: {unpacked_data[0]}, Msg type: {unpacked_data[1]}, data: {unpacked_data[2:64]}')
-    
+
     # Parse contents. 4 byte unsigned int, then 8x32 bytes unsigned chars
     # Since there is extra space left on the last packet, truncate that so we can unpack exactly what we need
     unpacked_data = struct.unpack('<I' + 'B'*8*32, contents[0:8*32+4])
@@ -57,12 +65,19 @@ def process_wod(packets):
     print(time)
     # Write to a csv file
     with open('wod.csv', 'w', newline='') as csvfile:
-        header = ['time', 'mode', 'bat_voltage', 'bat_current', 'bus_3v_current', 'bus_5v_current', 'temp_comm', 'temp_eps', 'temp_battery']
+        header = ['time', 'mode', 'bat_voltage', 'bat_current', 'bus_3v_current',
+                  'bus_5v_current', 'temp_comm', 'temp_eps', 'temp_battery']
         writer = csv.writer(csvfile)
         writer.writerow(header)
         for i in range(0, 32):
-            time_column = [time + 60*i] # Increment by 1 minute for each iteration
+            # Increment by 1 minute for each iteration
+            time_column = [time + 60*i]
             data_row = [unpacked_data[8*i + j + 1] for j in range(8)]
+            # Check the mode value. It should only be 0 or 1
+            # If it is another value then set it to -1 because it's invalid
+            if data_row[0] > 1:
+                data_row[0] = -1
+
             # mode            = unpacked_data[8*i + 1]
             # bat_voltage     = unpacked_data[8*i + 2]
             # bat_current     = unpacked_data[8*i + 3]
@@ -75,9 +90,8 @@ def process_wod(packets):
             writer.writerow(time_column + data_row)
 
 
-
 # Open serial port
-ser = serial.Serial('COM7', 115200, timeout=1)
+ser = serial.Serial(COM_PORT, BAUD_RATE, timeout=1)
 
 # Start reading and writing threads
 read_thread = threading.Thread(target=serial_read, args=(ser,), daemon=True)
@@ -88,10 +102,9 @@ write_thread.start()
 
 # Keep the main thread alive
 try:
-    
+
     while True:
         time.sleep(1)
 except KeyboardInterrupt:
     print("Exiting...")
     ser.close()
-
